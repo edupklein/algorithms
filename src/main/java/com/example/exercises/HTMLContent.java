68ee5ae840267456a2ca2bc07c43401b8db0a1e7
Eduardo Klein <eduardo.klein@sap.com>
2025-09-14 16:43:55 -0300

    HTMLContent.java

---
+++ src/main/java/com/example/exercises/HTMLContent.java
@@ -0,0 +1,60 @@
+package com.example.exercises;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class HTMLContent{
+    public static void main(String[] args){
+
+        String input = "<h1>Nayeem loves counseling</h1>\n" +
+                "<h1><h1>Sanjay has no watch</h1></h1><par>So wait for a while<par>\n" +
+                "<Amee>safat codes like a ninja</amee>\n" +
+                "<SA premium>Imtiaz has a secret crash</SA premium>";
+
+        String input2 =" <h1>some</h1>\n" +
+                "<h1>had<h1>public</h1></h1>\n" +
+                "<h1>had<h1>public</h1515></h1>\n" +
+                "<h1><h1></h1></h1>\n" +
+                "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n" +
+                ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n" +
+                "<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>\n" +
+                "<>hello</>\n" +
+                "<>hello</><h>dim</h>\n" +
+                "<>hello</><h>dim</h>>>>>";
+
+        String[] lines = input2.split("\\r?\\n");
+
+        for(int i = 0; i < lines.length; i++) {
+            String line = lines[i];
+            String regex = "<([\\w\\s]+)>([^<>]*)</\\1>";
+
+            Pattern p = Pattern.compile(regex);
+            List<String> contents = new ArrayList<>();
+
+            boolean found;
+            do {
+                Matcher m = p.matcher(line);
+                found = false;
+                while(m.find()) {
+                    if(!m.group(2).isEmpty()) {
+                        contents.add(m.group(2));
+                    }
+                    line = line.replace(m.group(), "");
+                    found = true;
+                }
+
+            }while(found);
+            if(contents.isEmpty()) {
+                System.out.println("None");
+            }else{
+                contents.forEach(s -> System.out.println(s));
+            }
+        }
+    }
+}
+
+
+
